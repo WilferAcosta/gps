9e99df919511308b2d6a8163b820d3b7f00e6192
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-22 18:44:56 -0500

    feat: :art: stylos
    agregue estylos a los bordes y aumente tamaño de los poquemones

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ function pokemon() {
             let pkmheight = (data.height);
             //creamos el titulo pokemos
             document.getElementById('pokemonMsg').innerHTML =`
-            <h1 style="color: green;">Pokemon</h1>`
+            <h1 style="color: black;">Pokemon</h1>`
             //agrego la imagen
             document.getElementById('pokemonSprite').src = data.sprites.front_default;
             //agrego h1 con el nombre del pokemon
             document.getElementById('pokemonName').innerHTML = `
-            <h1 style="color: white;">${pkmName}</h1>`
+            <h1 style="color: black;">${pkmName}</h1>`
             //agrego el ancho del pochemon
             document.getElementById('pokemonweight').innerHTML = `
-            <h1 style="color: white;">${pkmweight}</h1>`
+            <h1 style="color: black;">${pkmweight}</h1>`
             //agrego el alto del pokemon
             document.getElementById('pokemonheight').innerHTML = `
-            <h1 style="color: white;">${pkmheight}</h1>`
+            <h1 style="color: black;">${pkmheight}</h1>`
         })
         //pokemos 2
         fetch(url2)
@@ -34,18 +34,18 @@ function pokemon() {
             let pkmweight = (data.weight);
             let pkmheight = (data.height);
             document.getElementById('pokemonMsg2').innerHTML =`
-            <h1 style="color: green;">Pokemon</h1>`
+            <h1 style="color: black;">Pokemon</h1>`
 
             document.getElementById('pokemonSprite2').src = data.sprites.front_default;
 
             document.getElementById('pokemonName2').innerHTML = `
-            <h1 style="color: white;">${pkmName}</h1>`
+            <h1 style="color: black;">${pkmName}</h1>`
 
             document.getElementById('pokemonweight2').innerHTML = `
-            <h1 style="color: white;">${pkmweight}</h1>`
+            <h1 style="color: black;">${pkmweight}</h1>`
             
             document.getElementById('pokemonheight2').innerHTML = `
-            <h1 style="color: white;">${pkmheight}</h1>`
+            <h1 style="color: black;">${pkmheight}</h1>`
         })
         pokeAll();
 }
@@ -63,7 +63,7 @@ const capitalize = (s) => {
 
 async function pokeAll(){
     for(let i =1 ; i<=200; i++)
-    await    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
     .then (res => res.json())
     .then (data =>{
         console.log(data.name);
